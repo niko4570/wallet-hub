e9c45e9f89ffd2dc5ce3b627206718cc1606f8b5
niko <niko24561@gmail.com>
2026-02-09 12:53:52 +0800

    Align Expo peer dependencies for stability

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -62,7 +62,6 @@ config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(workspaceRoot, "node_modules"),
 ];
-config.resolver.disableHierarchicalLookup = true;
 config.resolver.extraNodeModules = {
   ...config.resolver.extraNodeModules,
   "@react-native/virtualized-lists": path.resolve(
